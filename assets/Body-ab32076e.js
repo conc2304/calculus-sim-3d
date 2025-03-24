2ec893d7f335a57e78b99f0edd43cb96025ca9d1
Jose Conchello <jose.conchello@gmail.com>
2025-03-24 15:26:58 +0000

    Updates

---
+++ assets/Body-ab32076e.js
@@ -0,0 +1,1 @@
+class i{constructor(s,o){this.scene=o,this.mesh=s,this.body=null}update(){this.mesh.position.copy(this.body.position),this.mesh.quaternion.copy(this.body.quaternion)}}export{i as P};
